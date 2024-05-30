f55bfb2d948aba48e10a0c2cb0a9bbadfec69dd0
pgomez2 <pgomez2@hawk.iit.edu>
2024-05-30 17:44:14 -0500

    creation of the makefile

---
+++ assign2_buffer_manager/buffer_mgr.c
@@ -0,0 +1,24 @@
+#include "buffer_mgr.h"
+#include "storage_mgr.h"
+
+
+// Buffer Manager Interface Pool Handling
+RC initBufferPool(BM_BufferPool *const bm, const char *const pageFileName, 
+		const int numPages, ReplacementStrategy strategy,
+		void *stratData){return RC_OK;}
+RC shutdownBufferPool(BM_BufferPool *const bm){return RC_OK;}
+RC forceFlushPool(BM_BufferPool *const bm){return RC_OK;}
+
+// Buffer Manager Interface Access Pages
+RC markDirty (BM_BufferPool *const bm, BM_PageHandle *const page){return RC_OK;}
+RC unpinPage (BM_BufferPool *const bm, BM_PageHandle *const page){return RC_OK;}
+RC forcePage (BM_BufferPool *const bm, BM_PageHandle *const page){return RC_OK;}
+RC pinPage (BM_BufferPool *const bm, BM_PageHandle *const page, 
+		const PageNumber pageNum){return RC_OK;}
+
+// Statistics Interface
+PageNumber *getFrameContents (BM_BufferPool *const bm){return RC_OK;}
+bool *getDirtyFlags (BM_BufferPool *const bm){return RC_OK;}
+int *getFixCounts (BM_BufferPool *const bm){return RC_OK;}
+int getNumReadIO (BM_BufferPool *const bm){return RC_OK;}
+int getNumWriteIO (BM_BufferPool *const bm){return RC_OK;}
